cedee696f05d136d70799aba5c51f00a738cccc9
sean-birmingham <sean.birmingham98@gmail.com>
2025-05-23 22:26:28 -0400

    Add asteroid and asteroid field classes with collision detection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,6 +12,11 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField() 
 
     Player.containers = (updatable, drawable)
    
@@ -23,6 +31,11 @@ def main():
                 return 
         
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
